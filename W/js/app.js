71bb65da3666a7682cdaaa766639aca71248670b
Zach <zacbenincasa@gamil.com>
2022-11-05 14:49:20 -0600

    added variables for the rest of the numbered dice types as well as all corrispoding numbers equalling 120 for each dice

--- W/js/app.js
+++ W/js/app.js
@@ -1,12 +1,52 @@
+// this is code for the d20 drop down equation
+
 let d20roll_mods = document.getElementsByClassName('d20numberselection')
-let drop = document.getElementById('Drop')
+let button_1 = document.getElementById('confirm')
+let user = document.getElementById('userd20')
 let int20 = ['1,2,3,4,5,6']
 
-function do_math(){
+
+
+
+
+function do_math20(){
     int20 * 20
     }
 
+// this code is for the d-12
+let int12 = ['1,2,3,4,5,6,7,8,9,10']
 
+function do_math12(){
+
+ }
+
+// this code is for the d-10
+let int10 = ['1,2,3,4,5,6,7,8,9,10,11,12']
+
+
+
+
+
+
+// this code is for the d-8
+let int8 =['1,2,3,4,5,6,7,8,9,10,11,12,13,14,15']
+
+
+
+
+
+
+
+// this code is for the d-6
+let int6 = ['1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20']
+
+
+
+
+
+// this code is for the d-4
+
+let int4 = ['1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30']
 
 
 
