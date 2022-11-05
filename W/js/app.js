9a31c390eb816620f7d5abe45242a02db2843fb8
Zach <zacbenincasa@gamil.com>
2022-11-05 13:52:56 -0600

    added more html for the d20, also did the equation to calculate how many d20s user is going to input numbers totalling 120 for each number of dice, will work on universal equation for each so that will be less work in the future

---
+++ W/js/app.js
@@ -0,0 +1,13 @@
+let d20roll_mods = document.getElementsByClassName('d20numberselection')
+let drop = document.getElementById('Drop')
+let int20 = ['1,2,3,4,5,6']
+
+function do_math(){
+    int20 * 20
+    }
+
+
+
+
+
+
