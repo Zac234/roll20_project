cd321cf7b65817cd3ba38fd3819dc4b4dde5e6c2
Zach <zacbenincasa@gamil.com>
2022-11-05 15:51:52 -0600

    added sections for the other dice types and thinking about using scss for the img tags just to correctly space and size them

--- W/js/app.js
+++ W/js/app.js
@@ -2,28 +2,33 @@
 
 let d20roll_mods = document.getElementsByClassName('d20numberselection')
 let button_1 = document.getElementById('confirm')
-let user = document.getElementById('userd20')
-let int20 = ['1,2,3,4,5,6']
-
-
-
-
+let int20 = document.getElementById('userd20')
+int20 = ['1,2,3,4,5,6']
 
 function do_math20(){
-    int20 * 20
+    return(int20*20)
     }
+if (int20>=6){
+    button_1.addEventListener(do_math20)
+} else console.log('you didnt do it right this time')
+
+
+
+
 
 // this code is for the d-12
 let int12 = ['1,2,3,4,5,6,7,8,9,10']
 
 function do_math12(){
-
+    int12 * 12
  }
 
 // this code is for the d-10
 let int10 = ['1,2,3,4,5,6,7,8,9,10,11,12']
 
-
+function do_math10(){
+    int10 * 12
+}
 
 
 
@@ -31,7 +36,9 @@ let int10 = ['1,2,3,4,5,6,7,8,9,10,11,12']
 // this code is for the d-8
 let int8 =['1,2,3,4,5,6,7,8,9,10,11,12,13,14,15']
 
-
+function do_math8(){
+    int8 * 8
+}
 
 
 
@@ -40,7 +47,9 @@ let int8 =['1,2,3,4,5,6,7,8,9,10,11,12,13,14,15']
 // this code is for the d-6
 let int6 = ['1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20']
 
-
+function do_math6(){
+    int6 * 6
+}
 
 
 
@@ -48,6 +57,13 @@ let int6 = ['1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20']
 
 let int4 = ['1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30']
 
+function do_math4(){
+    int4 * 4
+}
 
 
 
+
+
+
+// this is the code for the total number from all dice used
